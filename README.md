4a33bbf729a9566ee27844e8f00a2f9c2340ed07
Simon Hanna <simon.a.hanna93@gmail.com>
2016-02-09 10:02:08 -0600

    Combined LED and NFC code into main

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # eBusPass-Arduino
 This is the code for communication between the Arduino and the emulated NFC card. The primary functionality is displaying text to and LCD screen, and initiating communication with the Android application.
+
+Please note, this code makes use of the SSD1306Ascii and PN532 libraries. Links for these pages can be found below.
+
+SSD1306: https://github.com/greiman/SSD1306Ascii
+PN532: https://github.com/Seeed-Studio/PN532
---
+++ main/main.ino
@@ -0,0 +1,142 @@
+//NFC Shield Libraries
+#include <PN532Interface.h>
+#include <PN532.h>
+#include <PN532_I2C.h>
+
+//OLED Libraries
+#include "SSD1306Ascii.h"
+#include "SSD1306AsciiAvrI2c.h"
+
+//NFC Shield Global Vars
+PN532_I2C pn532i2c(Wire);
+PN532 nfc(pn532i2c);
+
+//OLED Global Var
+#define I2C_ADDRESS 0x3C
+SSD1306AsciiAvrI2c oled;
+
+void setup() {
+
+  Serial.begin(115200);
+
+  //Init OLED
+  oled.begin(&Adafruit128x64, I2C_ADDRESS);
+  oled.setFont(Adafruit5x7);
+  oled.clear();
+
+
+  //Init NFC Shield
+  nfc.begin();
+
+  uint32_t versiondata = nfc.getFirmwareVersion();
+  if (! versiondata) {
+    Serial.print("Didn't find PN53x board");
+    while (1); // halt
+  }
+
+  Serial.print("Found chip PN5"); Serial.println((versiondata>>24) & 0xFF, HEX);
+
+  // configure board to read RFID tags
+  nfc.SAMConfig();
+
+}
+
+void loop()
+{
+  bool success;
+
+  uint8_t responseLength = 32;
+
+  Serial.println("Waiting for an ISO14443A card");
+
+  // set shield to inListPassiveTarget
+  success = nfc.inListPassiveTarget();
+
+  if(success) {
+
+     Serial.println("Found something!");
+
+    uint8_t selectApdu[] = { 0x00, /* CLA */
+                              0xA4, /* INS */
+                              0x04, /* P1  */
+                              0x00, /* P2  */
+                              0x07, /* Length of AID  */
+                              0xF0, 0x01, 0x02, 0x03, 0x04, 0x05, 0x16, /* AID defined on Android App */
+                              0x00  /* Le  */ };
+
+    uint8_t response[32];
+
+    success = nfc.inDataExchange(selectApdu, sizeof(selectApdu), response, &responseLength);
+
+    if(success) {
+
+      Serial.print("responseLength: "); Serial.println(responseLength);
+
+      nfc.PrintHexChar(response, responseLength);
+
+      do {
+        uint8_t apdu[] = "Hello from Arduino";
+        uint8_t back[32];
+        uint8_t length = 32;
+
+        success = nfc.inDataExchange(apdu, sizeof(apdu), back, &length);
+
+        if(success) {
+
+          Serial.print("responseLength: "); Serial.println(length);
+
+          nfc.PrintHexChar(back, length);
+        }
+        else {
+
+          Serial.println("Broken connection?");
+        }
+      }
+      while(success);
+    }
+    else {
+
+      Serial.println("Failed sending SELECT AID");
+    }
+  }
+  else {
+
+    Serial.println("Didn't find anything!");
+  }
+
+  delay(1000);
+}
+
+void printResponse(uint8_t *response, uint8_t responseLength) {
+
+   String respBuffer;
+
+    for (int i = 0; i < responseLength; i++) {
+
+      if (response[i] < 0x10)
+        respBuffer = respBuffer + "0"; //Adds leading zeros if hex value is smaller than 0x10
+
+      respBuffer = respBuffer + String(response[i], HEX) + " ";
+    }
+
+    Serial.print("response: "); Serial.println(respBuffer);
+}
+
+void setupNFC() {
+
+  nfc.begin();
+
+  uint32_t versiondata = nfc.getFirmwareVersion();
+  if (! versiondata) {
+    Serial.print("Didn't find PN53x board");
+    while (1); // halt
+  }
+
+  // Got ok data, print it out!
+  Serial.print("Found chip PN5"); Serial.println((versiondata>>24) & 0xFF, HEX);
+  Serial.print("Firmware ver. "); Serial.print((versiondata>>16) & 0xFF, DEC);
+  Serial.print('.'); Serial.println((versiondata>>8) & 0xFF, DEC);
+
+  // configure board to read RFID tags
+  nfc.SAMConfig();
+}
